2d27e8483c8dbcb167a8ec4487a6e3664c255eab
TonySoprano87 <l.pala87@gmail.com>
2026-07-15 07:36:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
     const oggi = new Date();
 
-    document.getElementById("today").innerHTML =
-        oggi.toLocaleDateString("it-IT", {
+    const data = document.getElementById("today");
+
+    if (data) {
+
+        data.textContent = oggi.toLocaleDateString("it-IT", {
             weekday: "long",
             day: "numeric",
             month: "long",
             year: "numeric"
         });
+
+    }
+
+    const stato = document.getElementById("stato");
+
+    if (stato) {
+
+        const vento = 8;
+        const onda = 0.20;
+
+        if (vento <= 10 && onda <= 0.30) {
+
+            stato.textContent = "🟢 USCITA CONSIGLIATA";
+
+        } else if (vento <= 15 && onda <= 0.50) {
+
+            stato.textContent = "🟡 ATTENZIONE";
+
+        } else {
+
+            stato.textContent = "🔴 MEGLIO RIMANDARE";
+
+        }
+
+    }
 
 });
